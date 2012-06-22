17605697c05404aee1c3fc6b06d78c35d6b46ddc
Cameron Johnston <cameron@needle.com>
2012-06-22 10:09:51 -0600

    adding group resource definitions to qa and deploy recipes

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,3 +1,8 @@
+group 'deploy' do
+  gid 2500
+  action :create
+end
+
 users_manage 'deploy' do
     group_id 2500
     action [ :remove, :create ]
--- recipes/qa.rb
+++ recipes/qa.rb
@@ -1,3 +1,8 @@
+group 'qa' do
+  gid 2400
+  action :create
+end
+
 users_manage 'qa' do
     group_id 2400
     action [ :remove, :create ]
